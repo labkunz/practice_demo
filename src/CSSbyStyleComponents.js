49f48b75953b5a469e1d50a16ccad9d5dd8fdce2
labkunz <bboykunz@gmail.com>
2021-12-09 14:50:27 +0800

    Try to use keyframes

--- src/CSSbyStyleComponents.js
+++ src/CSSbyStyleComponents.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import logo from './logo.svg';
 
 const StyledApp = styled.div`
@@ -17,9 +17,19 @@ const StyledHeader = styled.header`
     color: white;
 `;
 
+const rotate = keyframes`
+    from {
+        transform: rotate(0deg);
+    }
+    to {
+        transform: rotate(360deg);
+    }
+`;
+
 const StyledAppLogo = styled.img`
     height: 40vmin;
     pointer-events: none;
+    animation: ${rotate} infinite 20s linear;
 
     @media (prefers-reduced-motion: no-preference) {
         .App-logo {
@@ -42,14 +52,27 @@ const StyledAppLink = styled.a`
 `;
 
 const StyledP = styled.p`
-    ${(props) => `color: ${props.fontColor}`};
-    background-color: black;
+    ${(props) => `color: ${props.fontColor ? props.fontColor : "#fff"}`};
+    ${(props) => `background-color: ${props.backgroundColor ? props.backgroundColor : "black"}`};
 `;
 
 const StyledButton = styled.button`
     padding: 10px;
     border-radius: 5px;
     margin-top: 20px;
+`;
+
+const StyledSpecialButton = styled(StyledButton)`
+    background-color: black;
+    color: white;
+    border: 0px;
+    width: 100px;
+    transition: all 0.7s;
+
+    &:hover {
+        color: red;
+        width: 250px;
+    }
 `;
 
 function CheckFile() {
@@ -63,6 +86,16 @@ function Description() {
 function CSSbySC() {
     CheckFile()
     const [fontColor, setFontColor] = React.useState("#fff");
+    const [backgroundColor, setBackgroundColor] = React.useState("black");
+    // React.useEffect(() =>{
+    //     setInterval(() => {
+    //         if (backgroundColor === "black") {
+    //             setBackgroundColor("#61dafb");
+    //         } else {
+    //             setBackgroundColor("black");
+    //         }
+    //     }, 1000);
+    // }, [backgroundColor]);
     return (
         <StyledApp>
             <StyledHeader>
@@ -70,9 +103,14 @@ function CSSbySC() {
                 <Description />
                 <StyledP fontColor={fontColor}>測試變化一</StyledP>
                 <StyledButton onClick={() => {
-                    setFontColor("pink");
+                    if (fontColor === "#fff") {
+                        setFontColor("pink");
+                    } else {
+                        setFontColor("#fff");
+                    }
                 }}>更改字體顏色</StyledButton>
-                <StyledP>測試變化二</StyledP>
+                {/* <StyledP backgroundColor={backgroundColor}>測試變化二</StyledP> */}
+                <StyledSpecialButton>Special Button</StyledSpecialButton>
                 <StyledAppLink href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</StyledAppLink>
             </StyledHeader>
         </StyledApp>
